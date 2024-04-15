94b30db231372e1bb763b5db85c4312c94070ca1
kiibra <kibra99@gmail.com>
2024-04-14 20:55:19 -0400

    complete temp movies data file

---
+++ data/movie-data.js
@@ -0,0 +1,10 @@
+const movies = [
+  {name: 'The Glory', genre: 'thriller', watched: true, _id: 28334},
+  {name: 'The 100', genre: 'Sci-fi', watched: true, _id: 67468},
+  {name: 'Money Heist', genre: 'thriller', watched: false, _id: 90438},
+  {name: 'Scarface', genre: 'crime', watched: false, _id: 82903},
+]
+
+export {
+  movies
+}
